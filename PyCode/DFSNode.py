bc2dbbef3bca437c503e2dbb8db3bc1907b3ff98
Yee-Kun Jung <twoketchupplz@gmail.com>
2022-06-10 15:47:13 +0900

    Update DFSNode.py
    정상작동
    implicit wait 로 해결함

--- PyCode/DFSNode.py
+++ PyCode/DFSNode.py
@@ -9,7 +9,7 @@ import time
 mainPage = 'https://mitel.dimi.uniud.it/ichi/'
 
 opt = Options()
-# opt.add_argument('headless')
+opt.add_argument('headless')
 opt.add_argument('--blink-settings=imagesEnabled=false')
 s = Service('../BrowserDriver/chromedriver.exe')
 driver = webdriver.Chrome(service=s, options=opt)
@@ -67,42 +67,5 @@ def DFS(prntList: list[WebElement], prntSelector: str, paramListDepth: int):
 
 DFS(driver.find_elements(By.CSS_SELECTOR, value='#tree li[aria-level="1"]'), '#tree li[aria-level="1"]', 1)
 
-while(True):
-    pass
-
-
-# rootLiVal = '#tree li[aria-level="1"]'
-# rootLiList = driver.find_elements(By.CSS_SELECTOR, value=rootLiVal)
-# time.sleep(0.5)
-#
-# print("length:", len(rootLiList))
-# print()
-#
-# nth = 1
-# for li in rootLiList:
-#     # li = driver.find_element(By.CSS_SELECTOR, value=rootLiVal + getNextSelector(nth))
-#     nextCSS = 'li[aria-level="1"]:nth-of-type(' + str(cnt + 1) + ') li[aria-level="' + str(2) + '"]'
-#
-#
-#     anc = li.find_element(By.TAG_NAME, value='a')
-#     print("href: " + anc.get_attribute('href'))
-#     print("text: " + anc.text)
-#     print()
-#     time.sleep(1)  # headless 에서도 필요한가
-#
-#     webdriver.ActionChains(driver).double_click(anc).perform()
-#     time.sleep(1.1)  # data-delay = 1000
-#
-#     lv2LiList = driver.find_elements(By.CSS_SELECTOR, value=nextCSS)
-#     time.sleep(1)  # headless 에서도 필요한가
-#
-#     print("하위노드 크기", len(lv2LiList))
-#     print()
-#
-#     print("********************")
-#     for i in lv2LiList:
-#         atag = i.find_element(By.TAG_NAME, value='a')
-#         print(atag.text)
-#     print("********************")
-#
-#     cnt = cnt + 1
+# while(True):
+#     pass
