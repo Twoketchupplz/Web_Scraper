ec52bf7525180d12d02b1ae3d7aa5d7643fa7820
Yee-Kun Jung <twoketchupplz@gmail.com>
2022-06-15 00:56:30 +0900

    Update DFSNode.py
    원하는 메뉴와 저장 파일 이름을 지정
    ExtensionCodes 확인
    속도 개선 요구됨

--- PyCode/DFSNode.py
+++ PyCode/DFSNode.py
@@ -6,8 +6,6 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from openpyxl import Workbook
 
-import time
-
 
 def getNth(prnt_li_index: int):  # Get CSS Selector(Nth of Current List Item) after tag
     # index 는 1부터 시작
@@ -18,43 +16,73 @@ def getAriaLevel(aria_depth: int):  # Get CSS Selector(Child Li List)
     return ' li[aria-level="' + str(aria_depth) + '"]'
 
 
-def getFullTitle(depth: int):
-    # Excel; depth 와 맞는 col 에 full title 출력
-    pass
+def getWorkbook(sheet_name: str):
+    work_book = Workbook()
+    work_sheet = work_book.create_sheet(sheet_name)
+
+    return work_book, work_sheet
 
 
-def getFieldNum(key):
-    print(key)
-    num = {'ICHI code': 0, 'XCode': 1, 'Target': 2, 'Action': 3, 'Means': 4, 'Title': 5, 'ICHI descriptor': 6,
-           'Definition': 7,
-           'Index Terms': 8,
-           'Includes Notes': 9,
-           'Excludes': 10, 'Code also': 11, 'Excludes Notes': 12, 'Notes': 13, 'Original ICD uri': 14}.get(key)
-    # print(num)
+def clickMenu(btn_menu: WebElement, menu: str):  # menu: Interventions, Target, Action, Means, ExtensionCodes
+    if 'open' not in btn_menu.get_attribute('class'):  # 아직 열리지 않은 메뉴인 경우 클릭
+        webdriver.ActionChains(driver).click(btn_menu).perform()
+
+    trg_menu = btn_menu.find_element(By.CSS_SELECTOR, value='li[data-tsel="' + menu + '"]')
+
+    webdriver.ActionChains(driver).click(trg_menu).perform()
+    print("Menu " + menu + " Clicked")
+
+
+def getFieldList(key):  # key: Interventions, Target, Action, Means, ExtensionCodes
+    fields = {'Interventions': ['ICHI code', 'Target', 'Action', 'Means', 'ICHI descriptor', 'Definition',
+                                'Index Terms', 'Includes Notes', 'Code also', 'Excludes Notes'],
+              'Target': ['ICHI code', 'Target', 'Action', 'Means', 'Title', 'ICHI descriptor', 'Definition',
+                         'Index Terms', 'Excludes', 'Includes Notes', 'Code also', 'Excludes Notes', 'Mapping ICF'],
+              'Action': ['ICHI code', 'Target', 'Action', 'Means', 'Title', 'ICHI descriptor', 'Definition',
+                         'Index Terms', 'Excludes', 'Includes Notes', 'Code also', 'Excludes Notes'],
+              'Means': ['ICHI code', 'Target', 'Action', 'Means', 'Title', 'ICHI descriptor', 'Definition',
+                        'Index Terms', 'Excludes', 'Includes Notes', 'Code also', 'Excludes Notes'],
+              'ExtensionCodes': ['XCode', 'Title', 'Definition', 'Index Terms', 'Excludes', 'Notes',
+                                 'Original ICD uri']}
+    return fields.get(key)
+
+
+def getFieldDict(field_list: list[str]):
+    return dict(zip(field_list, range(len(field_list))))
+
+
+def getFieldNum(field_dict: dict, key: str):
+    # print("key:", key)
+    num = field_dict.get(key)
     return num
 
 
-def getRecord(tr_tuple: tuple[WebElement], t: str):
-    global wb, ws
+def setWorkBook(sheet_name: str):
+    work_book = Workbook()
+    work_sheet = work_book.create_sheet(sheet_name)
+    field_list = getFieldList(sheet_name)
+    work_sheet.append(field_list)
+    field_dict = getFieldDict(field_list)
+    return work_book, work_sheet, field_dict
+
+
+def appendRecord(tr_tuple: tuple[WebElement]):
+    global wb, ws, f_dict
     # Excel; 필드값을 비교해 같은 곳에 value 출력
-    record = ['' for _ in range(15)]
+    # print("size:", len(f_dict))
+    record = ['' for _ in range(len(f_dict))]
     for tr in tr_tuple:
         field = tr.find_element(By.CSS_SELECTOR, value="td:nth-of-type(1)").text
-        fieldNum = getFieldNum(field)
-
+        field_idx = getFieldNum(f_dict, field)
+        # print(field_idx)
         data = tr.find_element(By.CSS_SELECTOR, value="td:nth-of-type(2)").text
+        record[field_idx] = data
 
-        record[fieldNum] = data
-    # for i in record:
-    #     print(i, end=', ')
-    # print()
-
-    # result: list로 엑셀 데이터 삽입
     ws.append(record)
-    wb.save("ICHI_Beta_3.xlsx")
 
 
 def DFS(prnt_tuple: tuple[WebElement], prnt_selector: str, prnt_depth: int):
+    global wb
     li_nth = 1
     tab: str = '   ' * (prnt_depth - 1) + '+  '
 
@@ -74,7 +102,7 @@ def DFS(prnt_tuple: tuple[WebElement], prnt_selector: str, prnt_depth: int):
             print(tab + '- ' + str(li_nth) + '. ' + trg_anchor.text)  # print a
 
             tr_tpl = tuple(driver.find_elements(By.CSS_SELECTOR, value="#content tbody > tr"))
-            getRecord(tr_tpl, tab)
+            appendRecord(tr_tpl)
 
             li_nth = li_nth + 1
             continue
@@ -83,7 +111,7 @@ def DFS(prnt_tuple: tuple[WebElement], prnt_selector: str, prnt_depth: int):
             print(tab + str(li_nth) + '. ' + trg_anchor.text)  # print a; step 2~6
 
             tr_tpl = tuple(driver.find_elements(By.CSS_SELECTOR, value="#content tbody > tr"))
-            getRecord(tr_tpl, tab)
+            appendRecord(tr_tpl)
 
             next_selector: str = prnt_selector + getNth(li_nth) + getAriaLevel(prnt_depth + 1)
             child_tpl = tuple(driver.find_elements(By.CSS_SELECTOR, value=next_selector))
@@ -91,43 +119,21 @@ def DFS(prnt_tuple: tuple[WebElement], prnt_selector: str, prnt_depth: int):
 
             li_nth = li_nth + 1
 
-        # print("* * * * * * * LV." + str(prnt_depth) + " DFS End * * * * * * *")
-
-
-# def ChooseDropDownMenu:  # ul.dropdown-menu
-#     return
+        wb.save("ICHI_Beta_3.xlsx")
 
 
 # Workbook
-
-wb = Workbook()
-
-ws = wb.create_sheet('Means', 0)
-
-fields_interventions = ['ICHI code', 'Target', 'Action', 'Means', 'ICHI descriptor', 'Definition',
-                        'Index Terms', 'Includes Notes', 'Code also', 'Excludes Notes']
-
-fields_target = ['ICHI code', 'Target', 'Action', 'Means', 'Title', 'ICHI descriptor', 'Definition',
-                 'Index Terms', 'Excludes', 'Includes Notes', 'Code also', 'Excludes Notes', 'Mapping ICF']
-
-fields_action = ['ICHI code', 'Target', 'Action', 'Means', 'Title', 'ICHI descriptor', 'Definition',
-                 'Index Terms', 'Excludes', 'Includes Notes', 'Code also', 'Excludes Notes']
-
-fields_means = ['ICHI code', 'Target', 'Action', 'Means', 'Title', 'ICHI descriptor', 'Definition', 'Index Terms',
-                'Excludes', 'Includes Notes', 'Code also', 'Excludes Notes']
-
-fields_extension_codes = ['XCode', 'Title', 'Definition', 'Index Terms', 'Excludes', 'Notes', 'Original ICD uri']
-
-# ws.append()  # 알맞는 리스트를 집어넣는다
+menu = 'Means'  # set menu
+wb, ws, f_dict = setWorkBook(sheet_name=menu)
 
 # 파일이 이미 있다면 다음 번호로..
-wb.save("ICHI_Beta_3.xlsx")
+wb.save("ICHI_Beta_3_Mns.xlsx")
 
 # Web Crawling
 mainPage = 'https://mitel.dimi.uniud.it/ichi/'
 
 opt = Options()
-# opt.add_argument('headless')
+opt.add_argument('headless')
 opt.add_argument('--blink-settings=imagesEnabled=false')
 s = Service('../BrowserDriver/chromedriver.exe')
 driver = webdriver.Chrome(service=s, options=opt)
@@ -137,16 +143,7 @@ print("Chrome Driver Ready")
 print()
 driver.get(mainPage)
 
-dropdown_menu = driver.find_element(By.CSS_SELECTOR, value='#dropdown-tsel')
-if 'open' not in dropdown_menu.get_attribute('class'):  # 아직 열리지 않은 메뉴인 경우 클릭
-    webdriver.ActionChains(driver).click(dropdown_menu).perform()
-
-menu_interventions = dropdown_menu.find_element(By.CSS_SELECTOR, value='li[data-tsel="Interventions"]')
-menu_target = dropdown_menu.find_element(By.CSS_SELECTOR, value='li[data-tsel="Target"]')
-menu_action = dropdown_menu.find_element(By.CSS_SELECTOR, value='li[data-tsel="Action"]')
-menu_means = dropdown_menu.find_element(By.CSS_SELECTOR, value='li[data-tsel="Means"]')
-menu_extension_codes = dropdown_menu.find_element(By.CSS_SELECTOR, value='li[data-tsel="ExtensionCodes"]')
-
-webdriver.ActionChains(driver).click(menu_means).perform()
+clickMenu(driver.find_element(By.CSS_SELECTOR, value='#dropdown-tsel'), menu=menu)
 
 DFS(tuple(driver.find_elements(By.CSS_SELECTOR, value='#tree li[aria-level="1"]')), '#tree li[aria-level="1"]', 1)
+wb.save("ICHI_Beta_3.xlsx")
